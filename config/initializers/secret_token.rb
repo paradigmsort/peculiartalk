82fbff6ba2a69d132dc66995c983a1dd158aeca7
W. Owen Parry <paradigmsort@gmail.com>
2013-10-10 19:52:32 -0400

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peculiartalk::Application.config.secret_token = '58c810f0e20bc0aa56d0cd07bf43855272638593c6e624b5ecd0d4bc1d52843465fd95b1101e044666e3c5b89538bae25412f3ac5dde0fab354da389b424c3ed'
