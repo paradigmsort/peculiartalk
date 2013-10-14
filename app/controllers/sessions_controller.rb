a8386f4ae465f05080bd8f14375f7140ce731380
W. Owen Parry <paradigmsort@gmail.com>
2013-10-14 12:37:13 -0400

    authentication call

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
     def new
     end
+    def create
+        user = User.find_by_name(params[:session][:name])
+        if user and user.authenticate(params[:session][:password])
+            flash[:success] = "Signed in!"
+            render 'new'
+        else
+            flash[:error] = "Try again!"
+            render 'new'
+        end
+    end
 end
